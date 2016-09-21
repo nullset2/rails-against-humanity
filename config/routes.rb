fe3c3df4d59b85b4efefd4a29d7b0ecfe554c426
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-21 01:35:09 -0500

    re-adds sign-out routes for devise with omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,11 @@ Rails.application.routes.draw do
   get 'omniauth_callbacks/google_oauth2'
   get 'omniauth_callbacks/facebook'
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
+
+  devise_scope :user do
+    delete 'sign_out', :to => 'devise/sessions#destroy', :as => :destroy_user_session
+  end
+
   resources :answers, only: [:index, :show]
   resources :games, only: [:create], param: :slug
 
