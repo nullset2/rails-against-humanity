305cefdadeb1f972f6768456286abf6fe0f9905c
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-21 01:34:51 -0500

    adds attachment avatar to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   devise :registerable, :rememberable, :trackable, :omniauthable, omniauth_providers: [:twitter, :facebook, :google_oauth2]
   validates :nickname, uniqueness: true
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
