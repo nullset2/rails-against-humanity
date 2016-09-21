511d77b6ae84e42c8eb370e8ac05763760cd64df
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-21 00:53:05 -0500

    adds fields for omniauth to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160920235610) do
+ActiveRecord::Schema.define(version: 20160921053116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(version: 20160920235610) do
     t.inet     "last_sign_in_ip"
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.string   "provider"
+    t.string   "uid"
     t.index ["nickname"], name: "index_users_on_nickname", unique: true, using: :btree
   end
 
