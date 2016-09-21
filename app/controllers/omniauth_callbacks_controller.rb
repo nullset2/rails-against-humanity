af6a36160d13ff6e78bb2f2ecee51f749b379ea7
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-21 02:41:12 -0500

    facebook authentication works now too

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -9,6 +9,12 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def facebook
+    @user = User.from_omniauth_hash(request.env["omniauth.auth"])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+    end
   end
 
   def failure
