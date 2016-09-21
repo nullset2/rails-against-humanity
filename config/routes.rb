d312e00b674899dd6d618533bd890f4460f306d1
Alfredo Gallegos <j.gallegos.chavarria@accenture.com>
2016-09-21 14:04:37 -0500

    cleanup routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
   resources :round_question_answers
   resources :player_cards
-  resources :round_questions
-  resources :rounds
-  resources :game_entries
   get 'omniauth_callbacks/google_oauth2'
   get 'omniauth_callbacks/facebook'
   devise_for :users, controllers: { omniauth_callbacks: "omniauth_callbacks" }
@@ -11,7 +8,8 @@ Rails.application.routes.draw do
   devise_scope :user do
     delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
-
+  
+  resources :questions, only: [:index, :show]
   resources :answers, only: [:index, :show]
   resources :games, only: [:new, :create], param: :slug
 
